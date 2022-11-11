adb9608373a6844b062d4b7f20cfd3fbc90aa294
MirkoFarina <MirkoFarina@outlook.com>
2022-11-11 15:41:05 +0100

    [add] bonus

--- js/app.js
+++ js/app.js
@@ -4,17 +4,23 @@ createApp ({
     data(){
         return {
             emailGenerated: [],
-            nEmail: 10
+            nEmail: 10,
+            isInLoading: false
         }
     },
     methods: {
         getApi(){
+            this.isInLoading = true;
+            this.emailGenerated = [];
             for(let i = 0; i < this.nEmail; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then(email => {
                     this.emailGenerated.push(email.data.response);
                 })
             }
+        },
+        checkEmailGenerated(){
+            return this.emailGenerated.length == this.nEmail;
         }
     }
 }).mount('#app')
