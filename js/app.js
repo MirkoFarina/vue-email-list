1a5bc0fe7bbd7ddb03a167293a18d70e182b6044
MirkoFarina <MirkoFarina@outlook.com>
2022-11-11 15:00:28 +0100

    [add] stamp in page

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 const { createApp } = Vue;
+
 createApp ({
     data(){
         return {
-
+            emailGenerated: [],
+            nEmail: 10
+        }
+    },
+    methods: {
+        getApi(){
+            for(let i = 0; i < this.nEmail; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(email => {
+                    this.emailGenerated.push(email.data.response);
+                })
+            }
         }
     }
 }).mount('#app')
+
